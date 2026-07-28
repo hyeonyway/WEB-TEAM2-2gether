525cde5dd41692bd90ca9dd095f7fab1cf6e03eb
hyeonyway <kimhm1219@gmail.com>
2026-07-28 15:44:15 +0900

    feat: 현재 사용자 요청 해석 구현

---
+++ backend/src/main/java/com/dbidding/global/security/CurrentUserArgumentResolver.java
@@ -0,0 +1,31 @@
+package com.dbidding.global.security;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class CurrentUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+	private final CurrentUserProvider currentUserProvider;
+
+	@Override
+	public boolean supportsParameter(MethodParameter parameter) {
+		return parameter.hasParameterAnnotation(CurrentUser.class)
+			&& parameter.getParameterType().equals(Integer.class);
+	}
+
+	@Override
+	public Object resolveArgument(
+		MethodParameter parameter,
+		ModelAndViewContainer mavContainer,
+		NativeWebRequest webRequest,
+		WebDataBinderFactory binderFactory
+	) {
+		return currentUserProvider.getCurrentUserId();
+	}
+}
