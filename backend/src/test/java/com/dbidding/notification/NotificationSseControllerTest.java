980bca1e50cd603ef6f95b58ad7915775c01b662
haimin13 <gkals862@naver.com>
2026-08-03 16:33:01 +0900

    feat: 알림 SSE 스트림 컨트롤러 및 티켓 인증 연동
    /api/users/{userId}/notifications/stream은 이미 SseTicketAuthFilter에
    등록된 경로라 TicketProvider를 직접 다루지 않고 @CurrentUser만 쓴다.
    경로의 userId와 인증된 유저가 다르면 401로 끊는다.

---
+++ backend/src/test/java/com/dbidding/notification/NotificationSseControllerTest.java
@@ -0,0 +1,48 @@
+package com.dbidding.notification;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import com.dbidding.global.security.CurrentUserProvider;
+
+@WebMvcTest(NotificationSseController.class)
+class NotificationSseControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private NotificationSseConnectionManager connectionManager;
+
+    @MockitoBean
+    private CurrentUserProvider currentUserProvider;
+
+    @Test
+    void 경로의_유저와_인증된_유저가_같으면_스트림을_연결한다() throws Exception {
+        given(currentUserProvider.getCurrentUserId()).willReturn(1);
+        given(connectionManager.connect(1)).willReturn(new SseEmitter());
+
+        mockMvc.perform(get("/api/users/1/notifications/stream"))
+                .andExpect(request().asyncStarted());
+
+        verify(connectionManager).connect(1);
+    }
+
+    @Test
+    void 경로의_유저와_인증된_유저가_다르면_401을_반환한다() throws Exception {
+        given(currentUserProvider.getCurrentUserId()).willReturn(2);
+
+        mockMvc.perform(get("/api/users/1/notifications/stream"))
+                .andExpect(status().isUnauthorized());
+    }
+}
